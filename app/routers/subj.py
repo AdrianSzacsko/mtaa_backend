da9abd83ccd1e7815e13e7f947d7847dfec77f7a
MarkoStahovec <xstahovec@stuba.sk>
2022-03-23 17:10:24 +0100

    Added token requirement for subj.py

--- app/routers/subj.py
+++ app/routers/subj.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
 from starlette.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND
 
 from ..schemas import subj_schema
@@ -7,6 +7,7 @@ from sqlalchemy.orm import Session, aliased
 from sqlalchemy import func, union, select, or_, alias, text, and_
 from typing import List, Optional
 from ..models import *
+from ..security import auth
 
 router = APIRouter(
     prefix="/subj",
@@ -15,13 +16,21 @@ router = APIRouter(
 
 
 @router.get("/", response_model=List[subj_schema.GetSubjectId])
-def get_subject(db: Session = Depends(create_connection), subj_id: Optional[int] = 0):
+def get_subject(db: Session = Depends(create_connection),
+                subj_id: Optional[int] = 0,
+                user: User = Depends(auth.get_current_user)):
     Professor1 = aliased(Professor)
     Professor2 = aliased(Professor)
 
     result = db.query(Subject.id, Subject.name,
                       func.concat(Professor1.first_name, " ", Professor1.last_name).label("teachers"),
                       func.concat(Professor2.first_name, " ", Professor2.last_name).label("garant"))
+
+    if not result:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Subject with id:{subj_id} was not found."
+        )
 
     join_query = result.join(Relation, Subject.id == Relation.subj_id) \
         .join(Professor1, Relation.prof_id == Professor1.id) \
@@ -32,7 +41,9 @@ def get_subject(db: Session = Depends(create_connection), subj_id: Optional[int]
 
 
 @router.get("/{subj_id}/reviews", response_model=List[subj_schema.GetSubjectIdReviews])
-def get_subject(db: Session = Depends(create_connection), subj_id: Optional[int] = 0):
+def get_subject_reviews(db: Session = Depends(create_connection),
+                        subj_id: Optional[int] = 0,
+                        user: User = Depends(auth.get_current_user)):
     result = db.query(Subject.id,
                       SubjectReview.message,
                       SubjectReview.prof_avg,
@@ -40,6 +51,12 @@ def get_subject(db: Session = Depends(create_connection), subj_id: Optional[int]
                       SubjectReview.difficulty,
                       func.concat(User.first_name, " ", User.last_name).label("user_name"),
                       User.id)
+
+    if not result:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Subject review with id:{subj_id} was not found."
+        )
 
     join_query = result.join(SubjectReview, Subject.id == SubjectReview.subj_id) \
         .join(User, SubjectReview.user_id == User.id) \
@@ -49,14 +66,16 @@ def get_subject(db: Session = Depends(create_connection), subj_id: Optional[int]
 
 
 @router.post("/", status_code=HTTP_201_CREATED, response_model=subj_schema.PostSubjectId)
-async def add_subj_review(subj: subj_schema.PostSubjectId, db: Session = Depends(create_connection)):
+async def add_subj_review(subj: subj_schema.PostSubjectId,
+                          db: Session = Depends(create_connection),
+                          user: User = Depends(auth.get_current_user)):
     if len(subj.message) <= 2:
         raise HTTPException(
             status_code=HTTP_400_BAD_REQUEST,
             detail="Message too short!",
         )
 
-    subj_review = SubjectReview(**subj.dict())
+    subj_review = SubjectReview(user_id=user.id, **subj.dict())
 
     db.add(subj_review)
     db.commit()
@@ -66,7 +85,9 @@ async def add_subj_review(subj: subj_schema.PostSubjectId, db: Session = Depends
 
 
 @router.put("/", status_code=HTTP_201_CREATED, response_model=subj_schema.PostSubjectId)
-async def modify_subj_review(subj: subj_schema.PostSubjectId, db: Session = Depends(create_connection)):
+async def modify_subj_review(subj: subj_schema.PostSubjectId,
+                             db: Session = Depends(create_connection),
+                             user: User = Depends(auth.get_current_user)):
     if len(subj.message) <= 2:
         raise HTTPException(
             status_code=HTTP_400_BAD_REQUEST,
@@ -84,6 +105,13 @@ async def modify_subj_review(subj: subj_schema.PostSubjectId, db: Session = Depe
             status_code=HTTP_404_NOT_FOUND,
             detail="Review not found!",
         )
+
+    if query_row.user_id != user.id:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Unauthorized to perform this action."
+        )
+
     query.update(subj.dict())
     db.commit()
 
